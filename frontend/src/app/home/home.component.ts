0033c74ab4a4c7349726434b727c42718b920695
srkp-mrittika-roy <mrittika.roy@srkptechnologies.com>
2024-05-07 15:55:06 +0530

    product with swiper js slider updated

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,14 +1,47 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit , CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+
+interface Product {
+  _id: string;
+  productName: string;
+  productType: string;
+  price: number;
+  description: string;
+  imageUrl: string;
+  available: boolean;
+}
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent],
+  imports: [HeaderComponent,FooterComponent,CommonModule,HttpClientModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
-})
-export class HomeComponent {
+  styleUrl: './home.component.css',
+  schemas : [CUSTOM_ELEMENTS_SCHEMA]
 
+})
+export class HomeComponent implements OnInit{
+  data : Product[] = [];
+
+
+
+  constructor(private httpclient: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+      this.fetchData()
+  }
+
+  fetchData(){
+    this.httpclient
+    .get('http://localhost:3000/products')
+     .subscribe((data:any)=>{
+      this.data = data ;
+     })
+  }
 }
