991a10b0685e8808d9e80f570ca8a1d76def3c09
srkp-mrittika-roy <mrittika.roy@srkptechnologies.com>
2024-05-21 23:28:11 +0530

    login page updated

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -16,55 +16,87 @@ import { AuthService } from '../auth.service';
 })
 export class LoginComponent{
 
+  emptyEmail: boolean = false;
+  emptyPassword: boolean = false;
+
   constructor( 
     private router: Router,
     private authservice : AuthService,
     private formbuilder : FormBuilder) { }
 
   form : FormGroup = this.formbuilder.group({
-    userEmail : ['', Validators.required],
-    password  : ['', Validators.required]
+    userEmail: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
   })
 
   signIn(e: Event){
-    this.authservice.signIn(this.form.value).subscribe({
-      next:(response) =>{
-        const message = response.message.toString();
-          Swal.fire({
-            icon : 'success',
-            title: 'Success',
-            text :  message,
-            confirmButtonText: 'OK'
-          });
-          const token = response.token;
-          const userId = response.data._id;
-          const userName = response.data.userName;
-          const userEmail = response.data.userEmail;
+    e.preventDefault();
+    this.emptyEmail    = this.form.controls['userEmail'].hasError('required');
+    this.emptyPassword = this.form.controls['password'].hasError('required');
 
-        localStorage.setItem('token', token);
-        localStorage.setItem('user._id', userId);
-        localStorage.setItem('userName',userName);
-        localStorage.setItem('userEmail', userEmail);
-        console.log(token);
-        console.log(userId);
-        console.log((userName));
-        
-        
-        if(token){
-          this.router.navigate(['home']);
-        }else{
-          this.router.navigate(['/']);
+    if(this.form.valid){
+      this.authservice.signIn(this.form.value).subscribe({
+        next:(response) =>{
+          const message = response.message.toString();
+            Swal.fire({
+              icon : 'success',
+              title: 'Success',
+              text :  message,
+            });
+
+            
+            const token = response.token;
+            const userId = response.data._id;
+            const userName = response.data.userName;
+            const userEmail = response.data.userEmail;
+  
+          localStorage.setItem('token', token);
+          localStorage.setItem('user._id', userId);
+          localStorage.setItem('userName',userName);
+          localStorage.setItem('userEmail', userEmail);
+          // console.log(token);
+          // console.log(userId);
+          // console.log((userName));
+          
+          if(token){
+            this.router.navigate(['home']);
+          }
+
+        }, error: (error: HttpErrorResponse) => {
+          if (error.status === 400) {
+            Swal.fire({
+              icon: 'error',
+              title: 'Error',
+              text: 'User with this email already exists.',
+            });
+          } else if (error.status === 404) {
+            Swal.fire({
+              icon: 'error',
+              title: 'Error',
+              text: 'User not found',
+            });
+          } else if(error.status === 401){
+            Swal.fire({
+              icon: 'error',
+              title: 'Error',
+              text: 'Incorrect Password',
+            });
+          } else{
+            Swal.fire({
+              icon: 'error',
+              title: 'Error',
+              text: 'Internal Server error',
+            });
+          }
         }
-
-      },error(error: HttpErrorResponse){
-        Swal.fire({
-          icon: 'error',
-          title: 'error',
-          text: "Login failed",
-          confirmButtonText: 'OK'
-        })
-      }
-    })
+      });
+    }else {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Validation Error',
+        text: 'Please fill in all the required fields correctly.',
+      });
+    }
   }
  
 }
